a8bc345e39f320f985b57d70fda42b1e16013e17
Ray Kao <ray@peopleancode.com>
2023-07-26 21:51:15 -0400

    fix: issues since I've never been a strong C# Dev

--- src/semantic-kernel-sidecar/Controllers/SemanticSkillsController.cs
+++ src/semantic-kernel-sidecar/Controllers/SemanticSkillsController.cs
@@ -1,9 +1,13 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Skills.Core;
 using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.Planning;
 using Microsoft.SemanticKernel.Planning.Sequential;
+using Microsoft.SemanticKernel.SkillDefinition;
+using semantic_kernel_sidecar.models;
+
 
 namespace semantic_kernel_sidecar.Controllers;
 
@@ -13,17 +17,22 @@ public class SemanticSkillsController : ControllerBase
 {
     private readonly ILogger<SemanticSkillsController> _logger;
     private static readonly KernelBuilder _builder = new KernelBuilder();
-    private readonly List<SemanticSkill> _semanticSkillsList = new List<Dictionary<string, ISKFunction>>();
-
+    private readonly List<Dictionary<string, ISKFunction>> _semanticSkillsList = new List<Dictionary<string, ISKFunction>>();
     
     // This is the constructor for the SemanticSkillsController - it should auto load the skills from the /SemanticSkills directory which is mounted to the container
-    public SemanticSkillsController(ILogger<SemanticSkills> logger)
+    public SemanticSkillsController(ILogger<SemanticSkillsController> logger)
     {
         _logger = logger;
 
-        string directoryPath = "/SemanticSkills";
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var settingsFilePath = Path.Combine(baseDirectory, "settings.json");
 
-        var (useAzureOpenAI, model, azureEndpoint, apiKey, orgId) = Settings.LoadFromFile();
+        // Load the settings from the file
+        var settingsJson = File.ReadAllText(settingsFilePath);
+
+        var (useAzureOpenAI, model, azureEndpoint, apiKey, orgId) = JsonSerializer.Deserialize<(bool, string, string, string, string)>(settingsJson);
+
+        // var (useAzureOpenAI, model, azureEndpoint, apiKey, orgId) = Settings.LoadFromFile();
 
         if (useAzureOpenAI)
             _builder.WithAzureTextCompletionService(model, azureEndpoint, apiKey);
@@ -34,6 +43,9 @@ public class SemanticSkillsController : ControllerBase
 
         // Load native skill into the _kernel registry, sharing its functions with prompt templates
         var planner = new SequentialPlanner(_kernel);
+
+        var skillsDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var directoryPath = Path.Combine(skillsDirectory, "/SemanticSkills");
 
         string[] subdirectories = Directory.GetDirectories(directoryPath);
             
@@ -55,28 +67,51 @@ public class SemanticSkillsController : ControllerBase
 
     // this shoudl return all the skills in the /SemanticSkills directory and any that have been created via the CreateNewSkill method
     [HttpGet(Name = "GetSemanticSkills")]
-    public IEnumerable<SemanticSkill> Get()
+    public IEnumerable<Dictionary<string, ISKFunction>> GetSemanticSkills()
     {
         return _semanticSkillsList;
     }
 
-    [HttpPost("{skillName}/{subskillName}", Name = "Invoke")]
-    public IActionResult Invoke(string skillName, string subskillName)
+    [HttpPost("{skillName}/{subSkillName}", Name = "Invoke")]
+    public async Task<IActionResult> InvokeSkill(string skillName, string subSkillName, [FromBody] SemanticSkill requestBody)
     {
+    
+        // // TODO: Implement Invoke method
+        // // This should be able to call the specific skill and subskill
+        
+        try {
+            var skill = _semanticSkillsList.Find(s => s.ContainsKey(skillName));
+            if (skill == null)
+            {
+                return NotFound($"Skill '{skillName}' not found");
+            }
 
-        // TODO: Implement Invoke method
-        // This should be able to call the specific skill and subskill
-        return null;
+            var subskill = skill[skillName];
 
+            if (subskill == null)
+            {
+                return NotFound($"Subskill '{subSkillName}' not found for skill '{skillName}'");
+            }
+
+            var userQuestion = requestBody.userQuestion.ToString();
+
+            var result = await subskill.InvokeAsync(userQuestion);
+
+            return Ok(result);
+        }
+        catch (Exception ex) 
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
     }
 
-    [HttpPost(Name = "CreateNewSkill")]
-    public IEnumerable<SemanticSkill> Get()
+    [HttpPost(Name = "CreateSkill")]
+    public IActionResult CreateSkill()
     {
         // TODO: Implement createnewskill method
         // This should allow the developer to post a JSON object to create a new skill over http similar to creating an inline skill
         // This should also persist the skill to disk (i.e. save to the /SemanticSkills directory)
-        return null;
+        return Ok();
 
     }
 }
---
+++ src/semantic-kernel-sidecar/Models/SemanticSkill.cs
@@ -0,0 +1,6 @@
+namespace semantic_kernel_sidecar.models;
+
+public class SemanticSkill
+{
+    public string? userQuestion { get; set; }
+}
