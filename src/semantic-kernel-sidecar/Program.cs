1f139917f60b54bb5b48915bd319e217ed4fe297
Ray Kao <ray@peopleancode.com>
2023-07-27 02:47:12 -0400

    update: IoC dependency Injection for Kernel and settings load

--- src/semantic-kernel-sidecar/Program.cs
+++ src/semantic-kernel-sidecar/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.SemanticKernel;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the required services
+builder.Services.AddSingleton<IKernel>(provider =>
+{
+    var builder = new KernelBuilder();
+
+    var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    var settingsFilePath = Path.Combine(baseDirectory, "settings.json");
+
+    // Load the settings from the file
+    var settingsJson = File.ReadAllText(settingsFilePath);
+
+    var (model, apiKey, orgId) = JsonSerializer.Deserialize<(string, string, string)>(settingsJson);
+
+    // var (model, apiKey, orgId) = Settings.LoadFromFile();
+
+    builder.WithOpenAITextCompletionService(model, apiKey, orgId);
+
+    IKernel kernel = builder.Build();
+
+    return kernel;
+});
 
 var app = builder.Build();
 
