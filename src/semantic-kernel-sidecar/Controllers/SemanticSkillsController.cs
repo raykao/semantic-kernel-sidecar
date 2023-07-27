46e07c358706c7201c21c6319f851b1a4085d750
Ray Kao <ray@peopleancode.com>
2023-07-27 02:46:38 -0400

    fix: IoC for Kernel; Dictionary Type for SkillsList; Path Load for Dir

--- src/semantic-kernel-sidecar/Controllers/SemanticSkillsController.cs
+++ src/semantic-kernel-sidecar/Controllers/SemanticSkillsController.cs
@@ -1,4 +1,4 @@
-using System.Text.Json;
+using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Skills.Core;
@@ -8,7 +8,6 @@ using Microsoft.SemanticKernel.Planning.Sequential;
 using Microsoft.SemanticKernel.SkillDefinition;
 using semantic_kernel_sidecar.models;
 
-
 namespace semantic_kernel_sidecar.Controllers;
 
 [ApiController]
@@ -16,102 +15,113 @@ namespace semantic_kernel_sidecar.Controllers;
 public class SemanticSkillsController : ControllerBase
 {
     private readonly ILogger<SemanticSkillsController> _logger;
-    private static readonly KernelBuilder _builder = new KernelBuilder();
-    private readonly List<Dictionary<string, ISKFunction>> _semanticSkillsList = new List<Dictionary<string, ISKFunction>>();
+    // private static readonly KernelBuilder _builder = new KernelBuilder();
+
+    private readonly IKernel _kernel;
+
+    // private readonly List<Dictionary<string, ISKFunction>> _semanticSkillsList = new List<Dictionary<string, ISKFunction>>();
+    private Dictionary<string, Dictionary<string, ISKFunction>> _semanticSkillsList = new Dictionary<string, Dictionary<string, ISKFunction>>();
     
     // This is the constructor for the SemanticSkillsController - it should auto load the skills from the /SemanticSkills directory which is mounted to the container
-    public SemanticSkillsController(ILogger<SemanticSkillsController> logger)
+    public SemanticSkillsController(ILogger<SemanticSkillsController> logger, IKernel kernel)
     {
         _logger = logger;
 
-        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        var settingsFilePath = Path.Combine(baseDirectory, "settings.json");
+        _kernel = kernel;
 
-        // Load the settings from the file
-        var settingsJson = File.ReadAllText(settingsFilePath);
-
-        var (useAzureOpenAI, model, azureEndpoint, apiKey, orgId) = JsonSerializer.Deserialize<(bool, string, string, string, string)>(settingsJson);
-
-        // var (useAzureOpenAI, model, azureEndpoint, apiKey, orgId) = Settings.LoadFromFile();
-
-        if (useAzureOpenAI)
-            _builder.WithAzureTextCompletionService(model, azureEndpoint, apiKey);
-        else
-            _builder.WithOpenAITextCompletionService(model, apiKey, orgId);
-
-        var _kernel = _builder.Build();
-
-        // Load native skill into the _kernel registry, sharing its functions with prompt templates
         var planner = new SequentialPlanner(_kernel);
 
-        var skillsDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        var directoryPath = Path.Combine(skillsDirectory, "/SemanticSkills");
+        var skillsDirectory = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "SemanticSkills");
 
-        string[] subdirectories = Directory.GetDirectories(directoryPath);
+        // var skillsDirectory = Path.Combine(currentDirectory, "SemanticSkills");
+
+        string[] subdirectories = Directory.GetDirectories(skillsDirectory);
             
         foreach (string subdirectoryPath in subdirectories)
         {
             string skillName = Path.GetFileName(subdirectoryPath);
-            var skill = _kernel.ImportSemanticSkillFromDirectory(directoryPath, skillName);
+
+            var skill = _kernel.ImportSemanticSkillFromDirectory(skillsDirectory, skillName);
+
             if (skill is Dictionary<string, ISKFunction>)
             {
-                _semanticSkillsList.Add((Dictionary<string, ISKFunction>)skill);
-                Console.WriteLine($"loaded {skillName} from {directoryPath}");
+                _semanticSkillsList[skillName] = (Dictionary<string, ISKFunction>)skill;
             }
             else
             {
-                Console.WriteLine($"skipping {skillName} from {directoryPath} because it is not a SemanticSkill");
+                Console.WriteLine($"skipping {skillName} from {skillsDirectory} because it is not a SemanticSkill");
             }
         }
     }
 
     // this shoudl return all the skills in the /SemanticSkills directory and any that have been created via the CreateNewSkill method
     [HttpGet(Name = "GetSemanticSkills")]
-    public IEnumerable<Dictionary<string, ISKFunction>> GetSemanticSkills()
+    public async Task<IActionResult> GetSemanticSkills()
     {
-        return _semanticSkillsList;
-    }
-
-    [HttpPost("{skillName}/{subSkillName}", Name = "Invoke")]
-    public async Task<IActionResult> InvokeSkill(string skillName, string subSkillName, [FromBody] SemanticSkill requestBody)
-    {
-    
-        // // TODO: Implement Invoke method
-        // // This should be able to call the specific skill and subskill
+        // return a list of all the skills in _semanticSkillsList as a json object
+        return Ok(_semanticSkillsList);
         
-        try {
-            var skill = _semanticSkillsList.Find(s => s.ContainsKey(skillName));
-            if (skill == null)
-            {
-                return NotFound($"Skill '{skillName}' not found");
-            }
-
-            var subskill = skill[skillName];
-
-            if (subskill == null)
-            {
-                return NotFound($"Subskill '{subSkillName}' not found for skill '{skillName}'");
-            }
-
-            var userQuestion = requestBody.userQuestion.ToString();
-
-            var result = await subskill.InvokeAsync(userQuestion);
-
-            return Ok(result);
-        }
-        catch (Exception ex) 
-        {
-            return StatusCode(500, $"An error occurred: {ex.Message}");
-        }
     }
 
-    [HttpPost(Name = "CreateSkill")]
-    public IActionResult CreateSkill()
+    [HttpPost("invoke/{skillName}/{subSkillName}", Name = "Invoke")]
+    public async Task<IActionResult> InvokeSkill(string skillName, string subSkillName)
     {
-        // TODO: Implement createnewskill method
-        // This should allow the developer to post a JSON object to create a new skill over http similar to creating an inline skill
-        // This should also persist the skill to disk (i.e. save to the /SemanticSkills directory)
-        return Ok();
+        Console.WriteLine($"skillName: {skillName}");
 
+        var skill = _semanticSkillsList[skillName];
+
+        try {
+            var result = await skill[subSkillName].InvokeAsync("time travel to dinosaur age");
+
+            Console.WriteLine(result);
+
+            return Ok();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+
+            return StatusCode(500, $"An error occurred: {ex}");
+        }
+
+    
+        // // // TODO: Implement Invoke method
+        // // // This should be able to call the specific skill and subskill
+        
+        // try {
+        //     var skill = _semanticSkillsList.Find(s => s.ContainsKey(skillName));
+        //     if (skill == null)
+        //     {
+        //         return NotFound($"Skill '{skillName}' not found");
+        //     }
+
+        //     var subskill = skill[skillName];
+
+        //     if (subskill == null)
+        //     {
+        //         return NotFound($"Subskill '{subSkillName}' not found for skill '{skillName}'");
+        //     }
+
+        //     var userQuestion = requestBody.userQuestion.ToString();
+
+        //     var result = await subskill.InvokeAsync(userQuestion);
+
+        //     return Ok(result);
+        // }
+        // catch (Exception ex) 
+        // {
+        //     return StatusCode(500, $"An error occurred: {ex.Message}");
+        // }
     }
 }
+
+//     [HttpPost(Name = "CreateSkill")]
+//     public IActionResult CreateSkill()
+//     {
+//         // TODO: Implement createnewskill method
+//         // This should allow the developer to post a JSON object to create a new skill over http similar to creating an inline skill
+//         // This should also persist the skill to disk (i.e. save to the /SemanticSkills directory)
+//         return Ok();
+
+//     }
+// }
